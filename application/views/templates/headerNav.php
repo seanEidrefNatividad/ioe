fabd7ce6617ea6028c45dd9085f595f432edd1e1
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2023-05-18 22:58:01 +0800

    with logo

--- application/views/templates/headerNav.php
+++ application/views/templates/headerNav.php
@@ -1,5 +1,7 @@
 <div id="headerNav">
-    <div id="logo">logo</div>
+    <div id="logoDiv">
+        <img id="logo" src="<?=base_url();?>images/scentinelLogo.jpg" alt="Logo"><span id="logoName">&nbsp;Scentinel</span>
+    </div>
     <a href="<?=base_url().'index.php/i/logout'?>"><button class="btn_headerNav">logout</button></a>
 </div>
 
