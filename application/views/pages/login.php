f1fe3cb4003b0827d858d6d1b04bfd868f74cdba
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2023-05-19 09:26:44 +0800

    login done

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,35 +1,34 @@
-<div id="container">
-	<h1>Welcome to Scentinel</h1>
-
-	<div id="body">
-		<div class="login_div1">
-			<?= $flashdata ?>
-			<!-- <form action="../controller/login.php" method="post"> -->
-			<?php echo form_open(base_url() . 'index.php/login'); ?>
-			<div class="login_div2">
-				<input id="login_tb_username" class="login_tb" type="email" id="email" name="email" placeholder="email"
-					autocomplete="off" value="<?= set_value('email'); ?>" maxlength="50" required>
-				<input class="login_tb" type="password" id="password" name="password" placeholder="Password"
-					autocomplete="off" required>
-				<button class="login_btn btn btn-primary" type="submit" name="submit">Log In</button>
-			</div>
-			<?php echo form_close(); ?>
-			<div class="login_div3">
-				<hr>
-				
-				<!-- Button trigger register_modal -->
-				<button id="register_btn" type="button" class="login_btn btn btn-success" data-bs-toggle="modal"
-					data-bs-target="#register_modal">
-					<!-- add index.php/ -->
-					<a href="<?=base_url().'index.php/register'?>">
-						Create new account
-					</a>
-				</button>
-
-			</div>
-		</div>
+<div id="loginDiv">
+	<div id="logoDiv" class="justify-content-start" >
+		<img id="logo" src="<?= base_url(); ?>images/scentinelLogo.jpg" alt="Logo" style="margin: 0;"><span id="logoName">&nbsp;Scentinel</span>
 	</div>
+	<h2 id="loginHeader">Log in to your account</h2>
+	<p id="registerLink">Don't have an account? <a href="<?= base_url() . 'index.php/register' ?>">Sign Up</a> </p>
 
-	<p class="footer">
-	</p>
+	<div class="">
+		<?= $flashdata ?>
+
+		<?php echo form_open(base_url() . 'index.php/login'); ?>
+		<div class="">
+			<label class="lbl_login" for="email">Email Address</label>
+			<input class="tb_login" type="email" id="email" name="email" placeholder="email" autocomplete="off"
+				value="<?= set_value('email'); ?>" maxlength="50" required>
+
+			<label class="lbl_login" for="password">Password</label>
+			<input class="tb_login" type="password" id="password" name="password" placeholder="Password"
+				autocomplete="off" required>
+
+			<button id="btn_login" class="btn btn-success" type="submit" name="submit">Log In</button>
+		</div>
+		<?php echo form_close(); ?>
+
+	</div>
+</div>
+
+<p class="footer">
+</p>
+</div>
+
+<div id="loginDiv2">
+	<img id="loginBG" src="<?= base_url(); ?>images/bg.jpg" alt="bgimage">
 </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,15 +12,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
     <link href="https://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/euclid-circular-a" rel="stylesheet">
+    
     <link rel="stylesheet" href="<?=base_url();?>css/reset.css">
     <link rel="stylesheet" href="<?=base_url();?>css/style.css">
     <link rel="stylesheet" href="<?=base_url();?>css/headerNav.css">
     <link rel="stylesheet" href="<?=base_url();?>css/sidebarNav.css">
     <link rel="stylesheet" href="<?=base_url();?>css/task.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/login.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/register.css">
+
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-
 
 </head>
 <body>
